ca1bd61e8bf5d635b0ae81c8dd866207563884e3
Rajan <rajanpatel2684@gmail.com>
2024-02-03 15:18:07 +0530

    Banners completed

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -28,74 +28,74 @@ const Footer = () => {
             </div>
           </div>
         </div>
-        </footer>
-        <footer className='py-4'>
-          <div className='container-xxl'>
-            <div className="row">
+      </footer>
+      <footer className='py-4'>
+        <div className='container-xxl'>
+          <div className="row">
+            <div className="col-4">
+              <h4>Contact Us</h4>
+              <div className="footer-links d-flex flex-column">
+                <address className='mt-'>Project Store<br /> No.123, XYZ Mall, Gujarat, 987654<br />India
+                </address>
+                <a href="tel:+919873214560" className='text-white mb-2 mt-2'>+91 9873214560</a>
+                <a href="mailto:modernmart.noreply@gmail.com" className='text-white mb-2 mt-2'>modernmart.noreply@gmail.com</a>
+                <div className="social-profiles mt-3">
+                  <a href="/" className='text-white'><BsFacebook className='fs-2 me-3' /></a>
+                  <a href="/" className='text-white'><AiFillTwitterCircle className='fs-1 me-3' /></a>
+                  <a href="" className='text-white'><BsLinkedin className='fs-2 me-3' /></a>
+                  <a href="/" className='text-white'><FaGithub className='fs-2 me-3' /></a>
+                  <a href="/" className='text-white'><FaYoutube className='fs-1 me-3' /></a>
+                </div>
+              </div>
+            </div>
+            <div className="col-3">
+              <h4>Information</h4>
+              <div className="footer-links d-flex flex-column">
+                <Link className='text-white py-2 mb-1'>Privacy Policy</Link>
+                <Link className='text-white py-2 mb-1'>Return Policy</Link>
+                <Link className='text-white py-2 mb-1'>Shopping Policy</Link>
+                <Link className='text-white py-2 mb-1'>Terms & Services</Link>
+                <Link className='text-white py-2 mb-1'>Blogs</Link>
+              </div>
+            </div>
+            <div className="col-3">
+              <h4>Account</h4>
+              <div className="footer-links d-flex flex-column">
+                <Link className='text-white py-2 mb-1'>Search</Link>
+                <Link className='text-white py-2 mb-1'>About Us</Link>
+                <Link className='text-white py-2 mb-1'>Faq</Link>
+                <Link className='text-white py-2 mb-1'>Contact</Link>
+                <Link className='text-white py-2 mb-1'>Size Chart</Link>
+              </div>
+            </div>
+            <div className="col-2">
+              <h4>Quick Links</h4>
+              <div className="footer-links d-flex flex-column">
+                <Link className='text-white py-2 mb-1'>Laptops</Link>
+                <Link className='text-white py-2 mb-1'>Headphones</Link>
+                <Link className='text-white py-2 mb-1'>Tablets</Link>
+                <Link className='text-white py-2 mb-1'>Watches</Link>
+              </div>
+            </div>
+          </div>
+        </div>
+      </footer>
+      <footer className='py-4'>
+        <div className='py-2'>
+          <div className="container-xxl">
+            <div className="row d-flex justify-content-between">
+              <div className="col-3">
+                <p className='mb-0'>&copy; {new Date().getFullYear()}; Powered By Modern Mart</p>
+              </div>
               <div className="col-4">
-                <h4>Contact Us</h4>
-                <div className="footer-links d-flex flex-column">
-                  <address className='mt-'>Project Store<br/> No.123, XYZ Mall, Gujarat, 987654<br/>India
-                  </address>
-                  <a href="tel:+919873214560" className='text-white mb-2 mt-2'>+91 9873214560</a>
-                  <a href="mailto:modernmart.noreply@gmail.com" className='text-white mb-2 mt-2'>modernmart.noreply@gmail.com</a>
-                  <div className="social-profiles mt-3">
-                    <a href="/" className='text-white'><BsFacebook className='fs-2 me-3'/></a>
-                    <a href="/" className='text-white'><AiFillTwitterCircle className='fs-1 me-3'/></a>
-                    <a href="" className='text-white'><BsLinkedin className='fs-2 me-3'/></a>
-                    <a href="/" className='text-white'><FaGithub className='fs-2 me-3'/></a>
-                    <a href="/" className='text-white'><FaYoutube className='fs-1 me-3'/></a>
-                  </div>
-                </div>
-              </div>
-              <div className="col-3">
-                <h4>Information</h4>
-                <div className="footer-links d-flex flex-column">
-                  <Link className='text-white py-2 mb-1'>Privacy Policy</Link>
-                  <Link className='text-white py-2 mb-1'>Return Policy</Link>
-                  <Link className='text-white py-2 mb-1'>Shopping Policy</Link>
-                  <Link className='text-white py-2 mb-1'>Terms & Services</Link>
-                  <Link className='text-white py-2 mb-1'>Blogs</Link>
-                </div>
-              </div>
-              <div className="col-3">
-                <h4>Account</h4>
-                <div className="footer-links d-flex flex-column">
-                  <Link className='text-white py-2 mb-1'>Search</Link>
-                  <Link className='text-white py-2 mb-1'>About Us</Link>
-                  <Link className='text-white py-2 mb-1'>Faq</Link>
-                  <Link className='text-white py-2 mb-1'>Contact</Link>
-                  <Link className='text-white py-2 mb-1'>Size Chart</Link>
-                </div>
-              </div>
-              <div className="col-2">
-                <h4>Quick Links</h4>
-                <div className="footer-links d-flex flex-column">
-                  <Link className='text-white py-2 mb-1'>Laptops</Link>
-                  <Link className='text-white py-2 mb-1'>Headphones</Link>
-                  <Link className='text-white py-2 mb-1'>Tablets</Link>
-                  <Link className='text-white py-2 mb-1'>Watches</Link>
-                </div>
-              </div>
-            </div>
-          </div>
-        </footer>
-        <footer className='py-4'>
-          <div className='py-2'>
-            <div className="container-xxl">
-              <div className="row d-flex justify-content-between">
-                <div className="col-3">
-                  <p className='mb-0'>&copy; {new Date().getFullYear()}; Powered By Modern Mart</p>
-                </div>
-                <div className="col-4">
 
-                </div>
               </div>
             </div>
           </div>
-        </footer>
-      </>
-      )
+        </div>
+      </footer>
+    </>
+  )
 }
 
-      export default Footer;
+export default Footer;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,78 @@
-import React from 'react'
-
+import React from 'react';
+const img1 = require('../images/img1.jpg');
+const img2 = require('../images/img2.jpg');
+const img3 = require('../images/img3.jpg');
+const { Link } = require('react-router-dom');
 const Home = () => {
   return (
-    <div>Home</div>
+    <>
+      <section className="home-wrapper-1 py-3">
+        <div className="container-xxl">
+          <div className="row">
+            <div className="col-6 rounded-3 p-0">
+              <div className="main-banner p-3 position-relative">
+                <img src={img1} alt="banner" className='image-fluid rounded-3' height={400} width={600} />
+                <div className="main-banner-content position-absolute">
+                  <h5>SUPERCHARGED BY PROS.</h5>
+                  <h2>lPad P53 Pro Max</h2>
+                  <h5>FROM $999 to $41.63/mo</h5>
+                  <h5>For 24 mon</h5>
+                  <div className="button button-primary"></div>
+                  <button type="button" className="btn btn-primary rounded-5 banner-button"><Link className='text-white'>Buy Now</Link></button>
+                </div>
+              </div>
+            </div>
+            <div className="col-6 d-flex flex-wrap justify-content-evenly align-items-center">
+            <div className="small-banner position-relative">
+                <img src={img2} alt="banner" className='image-fluid rounded-3' height={180} width={280} />
+                <div className="small-banner-content position-absolute">
+                  <h6>BEST SALE</h6>
+                  <h4>lPad P53 Pro Max</h4>
+                  <h6>FROM $999 to $41.63/mo</h6>
+                  <h6>For 24 mon</h6>
+                  <div className="button button-primary"></div>
+                  {/* <button type="button" className="btn btn-primary rounded-5 banner-button"><Link className='text-white'>Buy Now</Link></button> */}
+                </div>
+              </div>
+              <div className="small-banner position-relative">
+                <img src={img3} alt="banner" className='image-fluid rounded-3' height={180} width={280} />
+                <div className="small-banner-content position-absolute">
+                  <h6>NEW ARRIVAL</h6>
+                  <h4>lPad P53 Pro Max</h4>
+                  <h6>FROM $999 to $41.63/mo</h6>
+                  <h6>For 24 mon</h6>
+                  <div className="button button-primary"></div>
+                  {/* <button type="button" className="btn btn-primary rounded-5 banner-button"><Link className='text-white'>Buy Now</Link></button> */}
+                </div>
+              </div>
+              <div className="small-banner position-relative">
+                <img src={img2} alt="banner" className='image-fluid rounded-3' height={180} width={280} />
+                <div className="small-banner-content position-absolute">
+                  <h6>15% OFF</h6>
+                  <h4>lPad P53 Pro Max</h4>
+                  <h6>FROM $999 to $41.63/mo</h6>
+                  <h6>For 24 mon</h6>
+                  <div className="button button-primary"></div>
+                  {/* <button type="button" className="btn btn-primary rounded-5 banner-button"><Link className='text-white'>Buy Now</Link></button> */}
+                </div>
+              </div>
+              <div className="small-banner position-relative">
+                <img src={img3} alt="banner" className='image-fluid rounded-3' height={180} width={280} />
+                <div className="small-banner-content position-absolute">
+                  <h6>FREE ENGRAVING</h6>
+                  <h4>lPad P53 Pro Max</h4>
+                  <h6>FROM $999 to $41.63/mo</h6>
+                  <h6>For 24 mon</h6>
+                  <div className="button button-primary"></div>
+                  {/* <button type="button" className="btn btn-primary rounded-5 banner-button"><Link className='text-white'>Buy Now</Link></button> */}
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
   )
 }
 
-export default Home
+export default Home;
